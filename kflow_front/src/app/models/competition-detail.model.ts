8dfa7698033d99d1ab6285bf305fdb99c0c657bd
chinokira <adrisqf@gmail.com>
2025-04-29 15:59:52 +0200

    commit pour travailler sur lautre pc

--- kflow_front/src/app/models/competition-detail.model.ts
+++ kflow_front/src/app/models/competition-detail.model.ts
@@ -1,3 +1,4 @@
+import { Categorie } from "./categorie.model";
 export interface Competition {
   id: number;
   name: string;
@@ -5,34 +6,6 @@ export interface Competition {
   level: string;
   startDate: string;
   endDate: string;
-  categories: Categorie[];
+  categories?: Categorie[]|[];
 }
 
-export interface Categorie {
-  id: number;
-  name: string;
-  participants?: Participant[];
-  stages: Stage[];
-}
-
-export interface Participant {
-  id: number;
-  bibNb: number;
-  name: string;
-  club: string;
-  runs?: Run[];
-}
-
-export interface Run {
-  id: number;
-  duration: number;
-  score: number;
-  stageName: string;
-}
-
-export interface Stage {
-  id: number;
-  name: string;
-  nbRun: number;
-  rules: string;
-} 
